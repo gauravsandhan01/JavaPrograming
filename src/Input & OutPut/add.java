d45d0ce2f5a8afded5239a3779176f91e39c54fb
Gaurav Sandhan <gaurav.sandhan01@gmail.com>
2026-02-22 13:31:36 +0530

    Input file

---
+++ src/Input & OutPut/add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public  class add{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.print("Enter a Number:");
+        int num =sc.nextInt();
+
+        System.out.println(num);
+
+    }
+}
